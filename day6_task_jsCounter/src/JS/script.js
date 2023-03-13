cd57ad51ad4b6c08c44e45891d857a04a3812179
Ayush1790 <ayushgupta@cedcoss.com>
2023-03-13 12:16:37 +0530

    modifications done

--- day6_task_jsCounter/src/JS/script.js
+++ day6_task_jsCounter/src/JS/script.js
@@ -1,18 +1,14 @@
 function inc() {
-    var x=document.getElementById("cont").innerText;
+    let x=document.getElementById("count").innerText;
     x++;
-    if(x>9 && x%10==0)
-    {
-        alert("You reached "+x);
-    }
-    document.getElementById("cont").innerText=x;
+    document.getElementById("count").innerText=x;
 }
 
 function dec() {
-    var x=document.getElementById("cont").innerHTML;
+    let x=document.getElementById("count").innerHTML;
     if(x!=0)
     {
         x--;
-        document.getElementById("cont").innerText=x;
+        document.getElementById("count").innerText=x;
     }
 }
