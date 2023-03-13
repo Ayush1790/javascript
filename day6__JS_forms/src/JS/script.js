a41abcbaa0f71d9a07ca91f65b76599381e76b7c
Ayush1790 <ayushgupta@cedcoss.com>
2023-03-13 17:45:36 +0530

    final commit

--- day6__JS_forms/src/JS/script.js
+++ day6__JS_forms/src/JS/script.js
@@ -1,15 +1,17 @@
 function fun() {
     const age = document.getElementById("age").value;
     const weight = document.getElementById("weight").value;
-    if(weight>300)
+    //condition for wrong weight input
+    if(weight>1000)
     {
         document.getElementById("msg").innerHTML="Please fill weight correctly."
         return;  
     }
+    // condition for negative input
     if(age<0 || weight<0 ){
         document.getElementById("msg").innerHTML="Please fill only positive value.";
-        return;
     }
+    // condition for range 5-7
     if(age>=5 && age<=7){
         if(weight>20){
             document.getElementById("msg").innerHTML="OverWeight.";
@@ -18,9 +20,11 @@ function fun() {
             document.getElementById("msg").innerHTML="UnderWeight.";
         }
         else{
+            document.getElementById("msg").style.color="green";
             document.getElementById("msg").innerHTML="You are fit.";
         }
     }
+      // condition for range 8-10
     else if(age>=8 && age<=10){
         if(weight>25){
             document.getElementById("msg").innerHTML="OverWeight.";
@@ -32,6 +36,7 @@ function fun() {
             document.getElementById("msg").innerHTML="You are fit.";
         }
     }
+      // condition for range 11-15
     else if(age>=11 && age<=15){
         if(weight>30){
             document.getElementById("msg").innerHTML="OverWeight.";
@@ -43,6 +48,7 @@ function fun() {
             document.getElementById("msg").innerHTML="You are fit.";
         }
     }
+      // condition for range 16-20
     else if(age>=16 && age<=20){
         if(weight>40){
             document.getElementById("msg").innerHTML="OverWeight.";
@@ -61,7 +67,7 @@ function fun() {
         document.getElementById("msg").innerHTML="Please Input the age in the range of 5 to 20.";
     }
     else {
-        document.getElementById("msg").innerHTML="Please fill only positive value."
+        document.getElementById("msg").innerHTML="Incorrect Input!, Please fill Correctlly"
     }
 }
 
