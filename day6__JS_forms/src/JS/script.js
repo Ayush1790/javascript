d471d21a22498d4db9deb5579bfde0ca1d03a285
Ayush1790 <ayushgupta@cedcoss.com>
2023-03-13 19:07:50 +0530

    final commit

--- day6__JS_forms/src/JS/script.js
+++ day6__JS_forms/src/JS/script.js
@@ -1,77 +1,77 @@
 function fun() {
     const age = document.getElementById("age").value;
     const weight = document.getElementById("weight").value;
-    let c=document.getElementById("msg").innerHTML;
+   
     //condition for wrong weight input
     if(weight>1000)
     {
-        c="Please fill weight correctly."
+        document.getElementById("msg").innerHTML="Please fill weight correctly."
         return;  
     }
     // condition for negative input
     if(age<0 || weight<0 ){
-        c="Please fill only positive value.";
+        document.getElementById("msg").innerHTML="Please fill only positive value.";
     }
     // condition for range 5-7
     if(age>=5 && age<=7){
         if(weight>20){
-            c="OverWeight.";
+            document.getElementById("msg").innerHTML="OverWeight.";
         }
         else if(weight<15){
-            c="UnderWeight.";
+            document.getElementById("msg").innerHTML="UnderWeight.";
         }
         else{
             document.getElementById("msg").style.color="green";
-            c="You are fit.";
+            document.getElementById("msg").innerHTML="You are fit.";
         }
     }
       // condition for range 8-10
     else if(age>=8 && age<=10){
         if(weight>25){
-            c="OverWeight.";
+            document.getElementById("msg").innerHTML="OverWeight.";
         }
         else if(weight<21){
-            c="UnderWeight.";
+            document.getElementById("msg").innerHTML="UnderWeight.";
         }
         else{
             document.getElementById("msg").style.color="green";
-            c="You are fit.";
+            document.getElementById("msg").innerHTML="You are fit.";
         }
     }
       // condition for range 11-15
     else if(age>=11 && age<=15){
         if(weight>30){
-            c="OverWeight.";
+            document.getElementById("msg").innerHTML="OverWeight.";
         }
         else if(weight<26){
-            c="UnderWeight.";
+            document.getElementById("msg").innerHTML="UnderWeight.";
         }
         else{
             document.getElementById("msg").style.color="green";
-            c="You are fit.";
+            document.getElementById("msg").innerHTML="You are fit.";
         }
     }
       // condition for range 16-20
     else if(age>=16 && age<=20){
         if(weight>40){
-            c="OverWeight.";
+            document.getElementById("msg").innerHTML="OverWeight.";
         }
         else if(weight<31){
-            c="UnderWeight.";
+            document.getElementById("msg").innerHTML="UnderWeight.";
         }
         else{
             document.getElementById("msg").style.color="green";
-            c="You are fit.";
+            document.getElementById("msg").innerHTML="You are fit.";
         }
     }
     else if(age>=5 && age<=20){
-        c="Please enter only year";
+        document.getElementById("msg").innerHTML="Please enter only year";
     }
     else if(age<5 && age>20){
-        c="Please Input the age in the range of 5 to 20.";
+        document.getElementById("msg").innerHTML="Please Input the age in the range of 5 to 20.";
     }
     else {
-        c="Incorrect Input!, Please fill Correctlly"
+        document.getElementById("msg").innerHTML="Incorrect Input!, Please fill Correctlly"
     }
 }
 
